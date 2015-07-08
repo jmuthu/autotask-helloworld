dfed0a133ba02a7aea33ecdffed669fb035732dd
Manan Hora <horamanan@gmail.com>
2015-07-08 09:58:52 +0530

    create account

--- src/autotask/helloworld/HelloWorldClient.java
+++ src/autotask/helloworld/HelloWorldClient.java
@@ -1,11 +1,19 @@
-package autotask.helloworld;
+//package autotask.helloworld;
 
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.WebServiceRef;
 
 import net.autotask.atws.v1_5.ATWS;
+import net.autotask.atws.v1_5.ATWSResponse;
 import net.autotask.atws.v1_5.ATWSSoap;
 import net.autotask.atws.v1_5.ATWSZoneInfo;
+import net.autotask.atws.v1_5.CreateResponse;
+import net.autotask.atws.v1_5.Entity;
+import net.autotask.atws.v1_5.Field;
+import net.autotask.atws.v1_5.Account;
+import net.autotask.atws.v1_5.Create;
+import net.autotask.atws.v1_5.ArrayOfEntity;
+
 
 public class HelloWorldClient {
 	// Initiate the webservice
@@ -17,10 +25,43 @@ public class HelloWorldClient {
 		// Set the credentials for authentication
 		BindingProvider prov = (BindingProvider) port;
 		prov.getRequestContext().put(BindingProvider.USERNAME_PROPERTY,
-				"username");
+		                             "PPierce@OneBillSandbox.com");
 		prov.getRequestContext().put(BindingProvider.PASSWORD_PROPERTY,
-				"password");
+		                             "12345");
 		ATWSZoneInfo zone = port.getZoneInfo("username");
-		System.out.println(zone.getURL());
+		System.out.println(zone.getURL().hashCode() + "hhh");
+		System.out.println("hello");
+
+
+		Account account = new Account();
+		account.setAccountName("Aditya Jain");
+		account.setAccountNumber(39233203);
+		account.setId(0);
+		account.setAccountType(0);
+		account.setCountry("United States");
+		account.setOwnerResourceID(29682885);
+		account.setPhone("89999999");
+		account.setAccountType(2);
+		account.setActive(true);
+		account.setClientPortalActive(false);
+		account.setTaskFireActive(false);
+		account.setTaxExempt(false);
+		account.setCountryID(237);
+		account.setBillToAddressToUse(1);
+		account.setBillToCountryID(237);
+
+		net.autotask.atws.v1_5.Entity  accounttocreate = account;
+		net.autotask.atws.v1_5.Entity [] array = {accounttocreate};
+
+		ArrayOfEntity entityarray = new ArrayOfEntity();
+		entityarray.equals(accounttocreate);
+
+		Create create = new Create();
+
+		create.setEntities(entityarray);
+		ATWSResponse response = new ATWSResponse();
+		CreateResponse createresponse = new CreateResponse();
+		response = createresponse.getCreateResult();
+
 	}
 }
